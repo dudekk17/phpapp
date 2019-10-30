176446beefdcf69609f61503f2e4e05a7c1e844e
dudekk17 <anna.dudek@lo1.sandomierz.pl>
2019-10-30 09:42:07 +0100

    aplikacja php

---
+++ inc/functions.php
@@ -0,0 +1,48 @@
+<?php
+
+// $pages = array (
+//  'witam' => 'Witamy',
+//  'formularz' =>"Formularz",
+//  'klasa'=> 'Klasy'
+//   );
+
+// github.com/lo1cgsan/phpapp
+
+$ret = array(); // tablica asocjacyjna zawierajaca wyniki zapytan
+
+function get_menu($id) {
+ global $db, $ret;
+ db_query('SELECT * FROM menu', $ret);
+ //print_r($ret);
+ foreach ($ret as $k => $t) {
+ 	 echo'
+	 <li class="nav-item">
+	    <a class="nav-link" href="?id='.$t['plik'].'">'.$t['tytul'].'</a>
+	 </li>
+ 	 ';
+ }
+}
+
+
+function get_page_title($id){
+  global $ret;
+  foreach ($ret as $k => $t) {
+  	if($t['plik'] == $id){
+  		echo $t['tytul'];
+  		return;
+  	}
+  }
+  	//tytul domyslny
+   echo 'Aplikacja PHP';
+
+
+}
+
+function get_page_content($id) {
+	if (file_exists($id.'.html'))
+		include($id.'.html');
+	else
+		include('404.html');
+}
+
+?>
